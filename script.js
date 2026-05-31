6e84ff126e33dfdf1536d8e8b19a457e49579e89
Shalu yadav <saluyadav102@gmail.com>
2026-05-31 13:45:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+
+        let value = button.innerText;
+
+        if (value === "C") {
+            display.value = "";
+        }
+        else if (value === "=") {
+            try {
+                display.value = eval(display.value);
+            }
+            catch {
+                display.value = "Error";
+            }
+        }
+        else {
+            display.value += value;
+        }
+
+    });
+});
